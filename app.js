3abbb29fafdb7eb2b5e766ade443633a7bbd4e47
Abdulahi <abdulahiaden444@gmail.com>
2024-08-25 19:19:35 -0400

    set up the main file of server application

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require('node:path');
+const messageRouter = require("./routes/messageRouter");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/', messageRouter);
+
+
+app.listen(PORT, () => {
+    console.log("server is running");
+})
