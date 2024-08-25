c029817b769525b0934c26a16c1b7e3d5bce65e6
Abdulahi <abdulahiaden444@gmail.com>
2024-08-25 19:17:26 -0400

    view all of the messages

---
+++ routes/messageRouter.js
@@ -0,0 +1,12 @@
+
+const { Router } = require('express');
+const { getAllMessages, getNewMessageForm, createNewMessage, getSingleMessage } = require('../controllers/messageController');
+
+const messageRouter = Router();
+
+messageRouter.get('/', getAllMessages)
+messageRouter.get('/message/:id', getSingleMessage);
+messageRouter.get('/new', getNewMessageForm);
+messageRouter.post('/new', createNewMessage);
+
+module.exports = messageRouter;
